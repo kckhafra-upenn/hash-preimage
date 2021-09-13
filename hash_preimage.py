2cd280f14699963c7bb5ec3298360e54683ca74a
kckhafra <kckhafra@gmail.com>
2021-09-13 13:45:29 -0400

    took out print statement

--- hash_preimage.py
+++ hash_preimage.py
@@ -10,14 +10,8 @@ def hash_preimage(target_string):
     scale=16
     length = len(target_string)
     xBitsTotal=bin(int(hashlib.sha256(x).hexdigest(), scale))[2:]
-    # targetStringBits=bin(int(hashlib.sha256(target_string.encode("utf-8")).hexdigest(), scale))[2:]
-    # print("Target: ",target_string)
-    # print("xBitsTotal: ",xBitsTotal)
-    # print("Length: ",length)
-    # print("Length2: ",len(xBitsTotal))
     match=False
     newLen=len(xBitsTotal)-length
-    # print(newLen)
     while(match==False):
         xLastKbits=xBitsTotal[newLen:len(xBitsTotal)]
 
@@ -26,7 +20,6 @@ def hash_preimage(target_string):
         else:
             x = os.urandom(64)
             xBitsTotal=bin(int(hashlib.sha256(x).hexdigest(), scale))[2:]
-    print(x)
     return(x)
 
 # hash_preimage("01011100")
